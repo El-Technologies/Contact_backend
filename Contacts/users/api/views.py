e281cb28f803cee9616b7931f59369328fb4871c
Rita-mary Amakom <amakomritamary322@gmail.com>
2023-01-13 22:49:54 +0100

    deleted an unnecessary import

--- Contacts/users/api/views.py
+++ Contacts/users/api/views.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.models  import User
 from .serializers import RegistrationSerializer , ProfileSerializer
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
